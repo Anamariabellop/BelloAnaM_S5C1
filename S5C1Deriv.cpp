757b9465ac27dc39a7dc3c68b3baba0e11bd0ad4
Anamariabellop <am.bello@uniandes.edu.co>
2019-07-04 02:40:40 +0000

    cpp actualizado

--- S5C1Deriv.cpp
+++ S5C1Deriv.cpp
@@ -1,54 +1,90 @@
 #include<iostream>
-#include <vector>
-#include <math.h>
+#include<math.h>
+#include <fstream>
 
 using namespace std;
 
-float *derivada( float *funcion, float deltax, int numero);
 
-int main(){
-    int a;
-    int b;
+float* derivada(float arr[], float h , int n, string filename);
+
+int main()
+{
+    string filename1;
+    filename1 = "x.dat";
+    string filename2;
+    filename2= "cos.dat";
+    string filename3;
+    filename3= "derivada.dat";
+    
+    
+    float a;
+    float b;
     int N;
-    
-    cout << "Ingrese a y b " << endl;
+
+    cout << "Ingrese inicio y final intervalo" << endl;
+    cout << "Inicio" << endl;
     cin >> a;
+    cout << "Final" << endl;
     cin >> b;
-    cout << "Ingrese numero de puntos" << endl;
-    cin >>N;
-    double dx;
+    cout << "Numero de puntos" << endl;
+    cin >> N;
+
+    float* arreglo= new float [N];
+    float* arrcos = new float [N];
+
+    float dx= (b-a)/(N-1);
+    ofstream outfile;
+    outfile.open(filename1);
     
-    dx= (b - a)/(N-1);
-        
-    float arrx[N];
-    arrx[0]=a;
-    arrx[N]=b;
+    ofstream outfile2;
+    outfile2.open(filename2);
     
-    float arrcos[N];
+    ofstream outfile3;
+    outfile3.open(filename3);
     
-    for( int i=1; i<N-1; i++)
+    for(int i= a; i<=N ; i++ )
     {
-        arrx[i]=i;        
+        arreglo[i]= i;
+
+        if(i==N)
+            {
+                arreglo[i]= b;
+            }
+    }
+   
+    for (int i=a; i<=N; i++)
+    {
+        arrcos[i]= cos(arreglo[i]);
+    }
+
+    for (int i=1; i<=N; i++)
+    {
+    outfile << arreglo[i] << endl;
     }
     
-    for( int i=0; i<N; i++)
+    for (int i=1; i<=N; i++)
     {
-        arrcos[i]= cos(arrx[i]);
-    }    
-    
-    //*derivada();
-     return 0;   
-   
+     outfile2 << arrcos[i] << endl;   
+    }
+
+    outfile3<< derivada(arrcos, dx, N, filename3) << endl;
+    outfile.close();
+    outfile2.close();
+    return 0;
 }
 
-float * derivada(int *funcion, float deltax, int numero){
-    float *arrderivada;
-    arrderivada= new float [numero];
+float* derivada(float arr[], float h, int n, string filename3)
+{
+    float* deriva= new float [n];
+    ofstream outfile3;
+    outfile3.open(filename3);
     
-    for( int i=0; i<numero; i++)
-    {
-     arrderivada[i]=(funcion[i+1]-funcion[i-1])/(2*deltax);   
+     for(int i=0; i<=n; i++)
+     {
+         deriva[i] = (arr[i+1]-arr[i-1])/(2*h);
+         outfile3<< deriva[i] << endl;
     }
-    
-   return arrderivada; 
+
+    return deriva;
+    outfile3.close();
 }
