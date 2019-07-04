ec36587d223f1536cb8caeaffd7a20e03ac2f176
Anamariabellop <am.bello@uniandes.edu.co>
2019-07-04 14:11:53 +0000

    final con pdf

--- BelloAnaS5C1.mk
+++ BelloAnaS5C1.mk
@@ -1,5 +1,5 @@
 #makefile
-S5C1PLOT.png: x.dat cos.dat derivada.dat plotsS5C1.py S5C1Deriv.cpp
+S5C1PLOT.pdf: x.dat cos.dat derivada.dat plotsS5C1.py S5C1Deriv.cpp
 	python plotsS5C1.py
 %.dat: a.out
 	./a.out
