30951affc3f643e77a07e78bb28466f6c7315e52
Anamariabellop <am.bello@uniandes.edu.co>
2019-07-04 14:13:30 +0000

    cambiado a pdf

--- plotsS5C1.py
+++ plotsS5C1.py
@@ -5,15 +5,15 @@ datos= np.genfromtxt("x.dat")
 datos2=np.genfromtxt("cos.dat")
 datos3=np.genfromtxt("derivada.dat")
 
-x=datos[1:]
-arrcos=datos2[1:]
+x=datos
+arrcos=datos2
 deriv=datos3[2:]
 
 plt.figure()
-plt.plot(x,deriv, color= 'r', label="F'(x)")
-plt.plot(x,arrcos, color= 'g', label= 'F(x)')
+plt.plot(x[1:-1],deriv, color= 'r', label="F'(x)")
+plt.plot(x[1:-1],arrcos[1:-1], color= 'g', label= 'F(x)')
 plt.legend()
 plt.title("f(x) y f'(x)")
 plt.xlabel("x")
 plt.ylabel("F(x),F'(x)")
-plt.savefig("S5C1PLOT.png")
+plt.savefig("S5C1PLOT.pdf")
